47a1aefbb0f74badec411b814d11807bd0b70777
jagatrsinghtandon <jagtarsinghtandon@gmail.com>
2018-06-07 15:05:56 +0200

    Update done

--- AuPairPlacementSystem/src/main/java/userProfileOperations/UserProfileOperations.java
+++ AuPairPlacementSystem/src/main/java/userProfileOperations/UserProfileOperations.java
@@ -10,13 +10,13 @@ public class UserProfileOperations {
 
 		try {
 
-			//test
+			// test
 			String PERSON_TYPE = "HOST";
 			Connection conn = null;
-			
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/AU_PAIR_MANAGEMENT?useSSL=false",
-			"root", "password123");
-			
+
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/AU_PAIR_MANAGEMENT?useSSL=false", "root",
+					"password123");
+
 			String query = "{CALL getProfileDetails(?, ?)}";
 			CallableStatement statement = conn.prepareCall(query);
 
@@ -40,76 +40,72 @@ public class UserProfileOperations {
 			String lastOnline = "";
 			String title = "";
 			String passportNo = "";
-			
+
 			Boolean hasValidVisa = false;
 			Boolean hasSalaryExpectation = false;
 			Boolean hasDrivingLicense = false;
 			String hobbies = "";
 			String supervisesChildOfAge = "";
 			String eduQualification = "";
-		
+
 			Boolean isSalaryProvided = false;
 			int numberOfKids = 0;
 			int ageOfKids = 0;
-			Boolean hasPyhsicalDisability = true; 
+			Boolean hasPyhsicalDisability = true;
 			String aboutMe = "";
-							
-			
-			 firstName = rs.getString("FIRST_NAME");
-			 lastName = rs.getString("LAST_NAME");
-			 contactNo = rs.getInt("CONTACT_NO");
-			 gender = rs.getString("GENDER");
-			 maritalStatus = rs.getString("MARITAL_STATUS");
-			 dob = rs.getDate("DOB"); /// check DOB Date time variable
-			 isActive = rs.getBoolean("IS_ACTIVE"); // check boolean type
-			 addressLine1 = rs.getString("ADDRESS_LINE1");
-			 city = rs.getString("CITY");
-			 postcode = rs.getInt("POSTCODE");
-			 countryName = rs.getString("COUNTRY_NAME");
-			 lastOnline = rs.getString("LAST_ONLINE");
-			 title = rs.getString("TITLE");
-			 passportNo = rs.getString("passport_no");
-			if(PERSON_TYPE=="AUPAIR")
-			{
-			 hasValidVisa = rs.getBoolean("HAS_VALID_VISA");
-			 hasSalaryExpectation = rs.getBoolean("HAS_SALARY_EXPECTATION");
-			 hasDrivingLicense = rs.getBoolean("HAS_DRIVING_LICENSE");
-			 hobbies = rs.getString("HOBBIES");
-			 supervisesChildOfAge = rs.getString("SUPERVISES_CHILD_OF_AGE");
-			 eduQualification = rs.getString("EDU_QUALIFICATION");
-			}
-			else{
-			 isSalaryProvided = rs.getBoolean("IS_SALARY_PROVIDED");
-			 numberOfKids = rs.getInt("NUMBER_OF_KIDS");
-			 ageOfKids = rs.getInt("AGE_OF_KIDS");
-			 hasPyhsicalDisability = rs.getBoolean("HAS_PHYSICAL_DISABILITY"); 
-			}// check boolean type
-			 aboutMe = rs.getString("ABOUT_ME");
+
+			firstName = rs.getString("FIRST_NAME");
+			lastName = rs.getString("LAST_NAME");
+			contactNo = rs.getInt("CONTACT_NO");
+			gender = rs.getString("GENDER");
+			maritalStatus = rs.getString("MARITAL_STATUS");
+			dob = rs.getDate("DOB"); /// check DOB Date time variable
+			isActive = rs.getBoolean("IS_ACTIVE"); // check boolean type
+			addressLine1 = rs.getString("ADDRESS_LINE1");
+			city = rs.getString("CITY");
+			postcode = rs.getInt("POSTCODE");
+			countryName = rs.getString("COUNTRY_NAME");
+			lastOnline = rs.getString("LAST_ONLINE");
+			title = rs.getString("TITLE");
+			passportNo = rs.getString("passport_no");
+			if (PERSON_TYPE == "AUPAIR") {
+				hasValidVisa = rs.getBoolean("HAS_VALID_VISA");
+				hasSalaryExpectation = rs.getBoolean("HAS_SALARY_EXPECTATION");
+				hasDrivingLicense = rs.getBoolean("HAS_DRIVING_LICENSE");
+				hobbies = rs.getString("HOBBIES");
+				supervisesChildOfAge = rs.getString("SUPERVISES_CHILD_OF_AGE");
+				eduQualification = rs.getString("EDU_QUALIFICATION");
+			} else {
+				isSalaryProvided = rs.getBoolean("IS_SALARY_PROVIDED");
+				numberOfKids = rs.getInt("NUMBER_OF_KIDS");
+				ageOfKids = rs.getInt("AGE_OF_KIDS");
+				hasPyhsicalDisability = rs.getBoolean("HAS_PHYSICAL_DISABILITY");
+			} // check boolean type
+			aboutMe = rs.getString("ABOUT_ME");
 
 			if (PERSON_TYPE == "AUPAIR") {
 				System.out.println("Profile Details are : \n1)First Name: " + firstName + "\n2)Last Name: " + lastName
 						+ "\n3)Contact No: " + contactNo + "\n4)Gender: " + gender + "\n5)Marital Status: "
 						+ maritalStatus + "\n6)Date Of Birth: " + dob + "\n7)Is Active User: " + isActive
 						+ "\n8)Address: " + addressLine1 + "\n9)City: " + city + "\n10)Postcode: " + postcode
-						+ "\n11)Country: " +countryName + "\n12)Last Online: " + lastOnline + "\n13)Title: " + title + "\n14)Passport Number: "
-						+ passportNo + "\n15)Has a Valid Visa: " + hasValidVisa + "\n16)Has Salary Expectations: "
-						+ hasSalaryExpectation + "\n17)Has Driving License: " + hasDrivingLicense + "\n18)Hobbies: "
-						+ hobbies + "\n19)Supervises Age of Chidren: " + supervisesChildOfAge
-						+ "\n20)Educational Qualification: " + eduQualification + "\n21) About me: " + aboutMe);
+						+ "\n11)Country: " + countryName + "\n12)Last Online: " + lastOnline + "\n13)Title: " + title
+						+ "\n14)Passport Number: " + passportNo + "\n15)Has a Valid Visa: " + hasValidVisa
+						+ "\n16)Has Salary Expectations: " + hasSalaryExpectation + "\n17)Has Driving License: "
+						+ hasDrivingLicense + "\n18)Hobbies: " + hobbies + "\n19)Supervises Age of Chidren: "
+						+ supervisesChildOfAge + "\n20)Educational Qualification: " + eduQualification
+						+ "\n21) About me: " + aboutMe);
 			} else if (PERSON_TYPE == "HOST") {
 				System.out.println("Profile Details are : \n1)First Name: " + firstName + "\n2)Last Name: " + lastName
 						+ "\n3)Contact No: " + contactNo + "\n4)Gender: " + gender + "\n5)Marital Status: "
 						+ maritalStatus + "\n6)Date Of Birth: " + dob + "\n7)Is Active User: " + isActive
 						+ "\n8)Address: " + addressLine1 + "\n9)City: " + city + "\n10)Postcode: " + postcode
-						+ "\n11)Country: " +countryName + "\n12)Last Online: " + lastOnline + "\n13)Title: " + title + "\n14)Passport Number: "
-						+ passportNo + "\n15)Is Salary Provided: " + isSalaryProvided + "\n16)Number Of Kids: "
-						+ numberOfKids + "\n17)Age of Kids: " + ageOfKids
+						+ "\n11)Country: " + countryName + "\n12)Last Online: " + lastOnline + "\n13)Title: " + title
+						+ "\n14)Passport Number: " + passportNo + "\n15)Is Salary Provided: " + isSalaryProvided
+						+ "\n16)Number Of Kids: " + numberOfKids + "\n17)Age of Kids: " + ageOfKids
 						+ "\n18)Does any of your kid have Physical Disability: " + hasPyhsicalDisability
 						+ "\n19)About me: " + aboutMe);
-				
 			}
 			mainmenu();
-
 		}
 
 		catch (Exception ex) {
@@ -131,25 +127,26 @@ public class UserProfileOperations {
 		} else if (userInput == 2) {
 			searchProfile();
 		} else if (userInput == 3) {
-////			SystString sql = "DELETE FROM PERSON WHERE PERSON_ID = PERSON_ID"; /// check ek baar
-//
-//				// statement.executeUpdate(sql); /////check check
-//				System.out.println("Thank you for being a part of Au-Pair Placement System");
-//
-//				// OR
-//
-//				// CallableStatement DELETE WALA FROM NIVEDITHA //)
-//			} else {
-//				System.out.println("Go"); // Ask niveditha about not deleting where to point it
-//			}
+			//// SystString sql = "DELETE FROM PERSON WHERE PERSON_ID = PERSON_ID"; ///
+			//// check ek baar
+			//
+			// // statement.executeUpdate(sql); /////check check
+			// System.out.println("Thank you for being a part of Au-Pair Placement System");
+			//
+			// // OR
+			//
+			// // CallableStatement DELETE WALA FROM NIVEDITHA //)
+			// } else {
+			// System.out.println("Go"); // Ask niveditha about not deleting where to point
+			//// it
+			// }
 		} else if (userInput == 4) {
 			// Ask niveditha for Proposals Do we need to call the Proposals Operations???
 		} else if (userInput == 5) {
-			////// LOGOUT FUNCTIONALITY ASK NIVEDITHA
+			////// LOGOUT FUNCTIONALITY 
 		}
 		sc.close();
 	}
-
 
 	private static void viewProposals() {
 
@@ -175,10 +172,10 @@ public class UserProfileOperations {
 	}
 
 	private static void updateProfile() throws SQLException {
-//
+		//
 		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/AU_PAIR_MANAGEMENT?useSSL=false",
 				"root", "password123");
-				
+
 		String firstname = "";
 		String lastname = "";
 		String email = "";
@@ -192,12 +189,11 @@ public class UserProfileOperations {
 
 		String query = "{CALL updateSelfProfile(?,?,?,?,?,?,?,?,?,?)}";
 		CallableStatement statementup = conn.prepareCall(query);
-		
-		
+
 		char wish = 'y';
 		int count = 0;
-		String updatedchanges ="";
-		while (wish == 'y' || wish == 'Y' ) {
+		String updatedchanges = "";
+		while (wish == 'y' || wish == 'Y') {
 			System.out.println("Which fields do you wish to Update? \n \nPress enter appropriate numbers: "
 					+ "\n1) First name \n2) Last name \n3) Email \n4) Contact \n5) About me \n6) Title \n7) Marital Status"
 					+ "\n8) Interview Time Slot \n9) Passport No.");
@@ -207,16 +203,15 @@ public class UserProfileOperations {
 				System.out.println("Please enter your first name: ");
 				firstname = sc.next();
 				count++;
-				updatedchanges+= "First Name: " +firstname;
-				
-				
+				updatedchanges += "First Name: " + firstname;
+
 				System.out.println("Do you wish to update more fields? Y/N ");
 				wish = sc.next().charAt(0);
 			} else if (updateSelf == 2) {
 				System.out.println("Please enter your last name: ");
 				lastname = sc.next();
 				count++;
-				updatedchanges+="\nLast Name: "+lastname;  
+				updatedchanges += "\nLast Name: " + lastname;
 
 				System.out.println("Do you wish to update more fields? Y/N ");
 				wish = sc.next().charAt(0);
@@ -224,7 +219,7 @@ public class UserProfileOperations {
 				System.out.println("Please enter your email: ");
 				email = sc.next();
 				count++;
-				updatedchanges+="\nEmail: "+email;
+				updatedchanges += "\nEmail: " + email;
 
 				System.out.println("Do you wish to update more fields? Y/N ");
 				wish = sc.next().charAt(0);
@@ -232,15 +227,15 @@ public class UserProfileOperations {
 				System.out.println("Please enter your Contact: ");
 				contact = sc.next();
 				count++;
-				updatedchanges+="\nContact: "+contact;
-			
+				updatedchanges += "\nContact: " + contact;
+
 				System.out.println("Do you wish to update more fields? Y/N ");
 				wish = sc.next().charAt(0);
 			} else if (updateSelf == 5) {
 				System.out.println("Please enter your About me section: ");
 				aboutme = sc.next();
 				count++;
-				updatedchanges+="\nAbout me:"+aboutme;
+				updatedchanges += "\nAbout me:" + aboutme;
 
 				System.out.println("Do you wish to update more fields? Y/N ");
 				wish = sc.next().charAt(0);
@@ -248,15 +243,16 @@ public class UserProfileOperations {
 				System.out.println("Please enter your Title: ");
 				title = sc.next();
 				count++;
-				updatedchanges+="\nTitle: "+title;
-			
+				updatedchanges += "\nTitle: " + title;
+
 				System.out.println("Do you wish to update more fields? Y/N ");
 				wish = sc.next().charAt(0);
 			} else if (updateSelf == 7) {
 				System.out.println("Please enter your Marital Status: ");
 				maritalstatus = sc.next();
 				count++;
-				updatedchanges+="\nMarital Status: "+maritalstatus;
+				//
+				updatedchanges += "\nMarital Status: " + maritalstatus;
 
 				System.out.println("Do you wish to update more fields? Y/N ");
 				wish = sc.next().charAt(0);
@@ -264,22 +260,22 @@ public class UserProfileOperations {
 				System.out.println("Please enter your Interview Time Slots: "); //// doubtsssss
 				interviewtimeslot = sc.next();
 				count++;
-				updatedchanges+="\nInterview Time Slot: "+interviewtimeslot;
+				updatedchanges += "\nInterview Time Slot: " + interviewtimeslot;
 
 				System.out.println("Do you wish to update more fields? Y/N ");
 				wish = sc.next().charAt(0);
 			} else if (updateSelf == 9) {
 				System.out.println("Please enter your Passport Number: ");
-				passportnumber= sc.next();
+				passportnumber = sc.next();
 				count++;
-				updatedchanges+="\nPassport number: "+passportnumber;
+				updatedchanges += "\nPassport number: " + passportnumber;
 
 				System.out.println("Do you wish to update more fields? Y/N ");
 				wish = sc.next().charAt(0);
 			}
 
 		}
-//
+		//
 		statementup.setInt("PERSONID", 1);
 		statementup.setString("FIRSTNAME", firstname);
 		statementup.setString("LASTNAME", lastname);
@@ -290,16 +286,14 @@ public class UserProfileOperations {
 		statementup.setString("MARITALSTATUS", maritalstatus);
 		statementup.setString("INTERVIEW_TIME_SLOT", interviewtimeslot);
 		statementup.setString("PASSPORT", passportnumber);
-		
 
 		statementup.execute();
-		
+
 		System.out.println(+count + " changes updated successfully");
-		System.out.println("\n" +updatedchanges);
-		
-			mainmenu();
-			sc.close();
+		System.out.println("\n" + updatedchanges);
+
+		mainmenu();
+		sc.close();
 	}
 
 }
-
