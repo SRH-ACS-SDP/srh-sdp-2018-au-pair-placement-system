5773e4b75cc0a6cf38dc504efb1824e1abf82a63
bhavana tiwari <bhavana.srh@gmail.com>
2018-06-07 21:34:44 +0200

    updated the packages names, and added utilies file

---
+++ AuPairPlacementSystem/src/main/java/edu/srh/aupair/utilities/utilities.java
@@ -0,0 +1,25 @@
+package edu.srh.aupair.utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class utilities {
+
+	public static void main(String[] args) {
+		
+		try {
+			getConnectionString();
+		} catch (SQLException e) {
+						e.printStackTrace();
+		}
+
+	}
+
+	public static Connection getConnectionString() throws SQLException {
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/AU_PAIR_MANAGEMENT?useSSL=false", "root",
+				"Pass123$$");
+		return conn;
+	}
+
+}
