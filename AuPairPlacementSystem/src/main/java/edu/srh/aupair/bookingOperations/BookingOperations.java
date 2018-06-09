cb94d1a88a783f97715f31d7b77e1ca401667f14
SelesAbraham <seles2112@gmail.com>
2018-06-09 22:01:30 +0200

    testing after commit, jenkins to build directly

--- AuPairPlacementSystem/src/main/java/edu/srh/aupair/bookingOperations/BookingOperations.java
+++ AuPairPlacementSystem/src/main/java/edu/srh/aupair/bookingOperations/BookingOperations.java
@@ -4,7 +4,7 @@ public class BookingOperations {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("testing jenkins");
+			System.out.println("Jenkins build operation succeed");
 	}
 
 }
