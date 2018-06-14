a87808f0705da886013a3c7556982c57ada527ae
SelesAbraham <seles2112@gmail.com>
2018-06-14 18:45:49 +0200

    Fixed Login bugs

--- AuPairPlacementSystem/src/main/java/edu/srh/aupair/loginOperations/Login.java
+++ AuPairPlacementSystem/src/main/java/edu/srh/aupair/loginOperations/Login.java
@@ -11,16 +11,53 @@ import org.mindrot.jbcrypt.BCrypt;
 public class Login {
 
 	public static void main(String[] args) throws SQLException {
-		loginWithUserCredentials();
+		
+		String userName = "";
+		String userPassword = "";
+		int the_count = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("***WELCOME TO AU PAIR PLACEMENT SYSTEM***\n");
+		
+		System.out.println("***PRESS 1 TO LOGIN AS HOST && PRESS 2 TO LOGIN AS AUPAIR*** \n");
+					
+		int loginAs = input.nextInt();
+		
+		
+		if(loginAs == 1)
+		{
+			System.out.println("Enter Username: ");
+			userName = input.next();
+			System.out.println("Enter Password: ");
+			userPassword = input.next();
+			String personType = "HOST";
 
+			loginUser(userName, userPassword, personType);
+		}
+		else if(loginAs == 2)
+		{
+			String personType = "AUPAIR";
+			loginUser(userName, userPassword,  personType);
+		}
+		else
+		{
+			System.out.println("***Incorrect user input***");
+			
+		}
+		
+		input.close();
+		
 	}
 
+
+
+		
 	public static void loginWithUserCredentials() throws SQLException {
 		String userName = "";
 		String userPassword = "";
 		int the_count = 0;
 		Scanner input = new Scanner(System.in);
 		System.out.println("***WELCOME TO AU PAIR PLACEMENT SYSTEM***\n");
+		
 		
 		System.out.println("***PRESS 1 TO LOGIN AS HOST && PRESS 2 TO LOGIN AS AUPAIR*** \n");
 					
@@ -60,7 +97,11 @@ public class Login {
 			}
 		}
 		else if(loginAs == 2)
-		{
+		{	
+			System.out.println("Enter Username: ");			
+			userName = input.next();
+			System.out.println("Enter Password: ");
+			userPassword = input.next();
 			String personType = "AUPAIR";
 			try {
 				String getEncryptedPassword = "{Call VerifyEncryptedPassword(?,?,?)}";
@@ -93,6 +134,46 @@ public class Login {
 		
 		input.close();
 		
+	}
+
+	
+	public static void loginUser (String userName, String userPassword,  String personType)
+	{
+		verifyLoginUser(userName,userPassword,personType);
+	}
+	
+	
+	
+	
+
+	public static void verifyLoginUser(String userName, String userPassword,  String personType) 
+	{
+		try {
+			
+			Connection conn = edu.srh.aupair.utilities.utilities.getConnectionString();
+			
+			
+			String getEncryptedPassword = "{Call VerifyEncryptedPassword(?,?,?)}";
+			CallableStatement cs = conn.prepareCall(getEncryptedPassword);
+			cs.setString(1, userName);
+			cs.setString(2, personType);
+			cs.registerOutParameter(3, Types.VARCHAR);
+			cs.execute();
+			String hashedPassword = cs.getString(3);
+			// System.out.println(hashedPassword);
+			if (BCrypt.checkpw(userPassword, hashedPassword)) {
+				System.out.println("Login successful");
+			}
+
+			else {
+				System.out.println("Failed to login...Try again with correct password");
+			}
+
+		}
+
+		catch (Exception ex) {
+			System.out.println("Failed to login...Try again with correct password");
+		}
 	}
 
 }
