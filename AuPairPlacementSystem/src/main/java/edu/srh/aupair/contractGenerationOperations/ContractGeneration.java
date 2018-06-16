7930fc87f325d4112e3191cdba9847bc911e37f4
nivedithasooda <nivsood@gmail.com>
2018-06-16 03:57:48 +0200

    Uploading contract file

--- AuPairPlacementSystem/src/main/java/edu/srh/aupair/contractGenerationOperations/ContractGeneration.java
+++ AuPairPlacementSystem/src/main/java/edu/srh/aupair/contractGenerationOperations/ContractGeneration.java
@@ -1,5 +1,8 @@
 package edu.srh.aupair.contractGenerationOperations;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,7 +23,7 @@ public class ContractGeneration {
 			
 			ResultSet rs = null;
 			
-			Connection conn = edu.srh.aupair.utilities.utilities.getConnectionString();
+			Connection connection = edu.srh.aupair.utilities.utilities.getConnectionString();
 			//Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/AU_PAIR_MANAGEMENT?useSSL=false", "root",
 				//	"Pass123$$");
     	   
@@ -30,7 +33,7 @@ public class ContractGeneration {
 					"interview_availability.INTERVIEW_ID=active_interviews.INTERVIEW_ID INNER JOIN au_pair ON \r\n" + 
 					"AU_PAIR.AU_PAIR_ID=interview_availability.AU_PAIR_ID;";
 			 
-			CallableStatement stmt = conn.prepareCall(query);
+			CallableStatement stmt = connection.prepareCall(query);
 			rs = stmt.executeQuery(query);
 			
 			//rs = (ResultSet) conn.prepareStatement("select Student_Name, Student_Address, Student_Contact,MatriculationNo from student");
@@ -57,7 +60,22 @@ public class ContractGeneration {
 			String currentUsersHomeDir = System.getProperty("user.home");
 			
 			JasperExportManager.exportReportToPdfFile(jp, currentUsersHomeDir + "/ContractForHostAndAuPair.pdf");	
-	
+
+			File image = new File(currentUsersHomeDir + "/ContractForHostAndAuPair.pdf");
+			/* prepareStatement() is used for create statement object that is 
+			used for sending sql statements to the specified database. */
+			CallableStatement stat = connection.prepareCall
+			("insert into contract(AU_PAIR_ID, HOST_ID, SIGNED_CONTRACT_IMAGE) "+ "values(?,?,?)");
+			stat.setString(1,"1");
+			stat.setString(2,"1");
+			FileInputStream fis = new FileInputStream(image);
+			stat.setBinaryStream(3, (InputStream)fis, (int)(image.length()));
+			/* executeUpdate() method execute specified sql query. Here this query 
+			insert data and image from specified address. */
+			int s = stat.executeUpdate();
+			if(s>0) {
+			System.out.println("Uploaded successfully !");
+			}
 					
 			
 		} catch (Exception ex) {
